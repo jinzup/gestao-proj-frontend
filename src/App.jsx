170eb7c27d914cc1b0e21be1dd33912761600226
jinzup <jinsgt@gmail.com>
2025-09-04 11:46:01 -0300

    Criação da tela de Login #master

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './components/login-page/login-main'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        {Login}
+        <Login />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/login-page/login-main.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import '../../css/login-main.css'; 
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setError('Por favor, preencha todos os campos!');
+      return;
+    }
+
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Gestão Projfix</h2>
+      {error && <div className="error">{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
